373d50b8ba6d42fed2e36b87f8ccf2646c116a4f
Philip Rosenberg-Watt <p.rosenberg-watt@cablelabs.com>
2019-06-12 14:57:17 -0600

    Ignore OS X cruft

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,21 @@
-FROM python:3
+FROM ubuntu
 
+ENV DEBIAN_FRONTEND "noninteractive"
 ENV MATRIX_APPSERVICE_LEDGER_CONFIG_DIR "/data"
 ENV MATRIX_APPSERVICE_LEDGER_CONFIG_FILE "ledger-registration.yaml"
 
+RUN apt-get update
+RUN apt-get install -y build-essential cmake doxygen libboost-system-dev libboost-dev python-dev gettext git libboost-date-time-dev libboost-filesystem-dev libboost-iostreams-dev libboost-python-dev libboost-regex-dev libboost-test-dev libedit-dev libgmp3-dev libmpfr-dev texinfo tzdata
+RUN git clone git://github.com/ledger/ledger.git
+RUN cd ledger && ./acprep update
+
+RUN apt-get install -y python3 python3-pip
 COPY install/requirements.txt .
 RUN pip install -r requirements.txt
 
 WORKDIR /app
 COPY app/ .
 
-VOLUME [ "/data" ]
+VOLUME [ "/data", "/ledger" ]
 
-ENTRYPOINT [ "python", "main.py" ]
+ENTRYPOINT [ "waitress-serve", "main:app" ]
--- app/main.py
+++ app/main.py
@@ -3,12 +3,15 @@ import os
 import subprocess
 
 import flask
+import requests
+import waitress
 import yaml
 from matrix_client.api import MatrixHttpApi
 
 CONFIG_DIR = os.environ.get("MATRIX_APPSERVICE_LEDGER_CONFIG_DIR", "../config/")
 CONFIG_FILE = os.environ.get("MATRIX_APPSERVICE_LEDGER_CONFIG_FILE", "ledger-registration.yaml")
 CONFIG_PATH = os.path.join(CONFIG_DIR, CONFIG_FILE)
+ALLOWED_USERS = os.environ.get("MATRIX_APPSERVICE_LEDGER_USERS", None).split(',')
 
 logging.basicConfig(
     format='%(asctime)s | %(levelname)-8s | %(threadName)-22s | %(message)s',
@@ -17,7 +20,7 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-if None in [CONFIG_DIR, CONFIG_FILE]:
+if None in [CONFIG_DIR, CONFIG_FILE, ALLOWED_USERS]:
     raise ValueError("Missing environment variable. Cannot continue.")
 
 with open(CONFIG_PATH, 'r') as stream:
@@ -34,34 +37,90 @@ with open(CONFIG_PATH, 'r') as stream:
 
 app = flask.Flask(__name__)
 matrix = MatrixHttpApi(HS_URL, token=AS_TOKEN)
-# matrix.sync()
 
 
 @app.route("/transactions/<txn_id>", methods=["PUT"])
 def on_receive_events(txn_id):
     events = flask.request.get_json()["events"]
     for event in events:
-        logger.info(f"User: {event['user_id']} Room: {event['room_id']}")
-        logger.info(f"Event Type: {event['type']}")
-        logger.info(f"Content: {event['content']}")
-        # logger.info(f"Room has {len(matrix.get_room_members(event['room_id']))} members in it.")
+        user_id = event['user_id']
+        room_id = event['room_id']
+        type = event['type']
+        content = event['content']
+        members = get_joined_room_members(room_id)
+
+        logger.debug(f"User: {user_id} Room: {room_id}")
+        logger.debug(f"Event Type: {type}")
+        logger.debug(f"Content: {content}")
+        logger.debug(f"The room has {len(members)} members in it: {', '.join(members.keys())}.")
+
+        if user_id in ALLOWED_USERS and type == 'm.room.message':
+            body = content['body']
+            if len(members) == 2:
+                data = None
+                if body.startswith('!echo '):
+                    message = content['body'][6:]
+                    logger.info(f"Echoing message '{message}' to room {room_id}")
+                    matrix.join_room(room_id)
+                    data = message_event(message)
+                elif body.startswith('!sh '):
+                    command = content['body'][4:]
+                    try:
+                        result = shell(command)
+                        data = message_event(result, f"<pre>{result}</pre>")
+                    except subprocess.CalledProcessError as cpe:
+                        logger.warning(cpe)
+                        data = message_event(f"command failed: {cpe}")
+                    except subprocess.TimeoutExpired as te:
+                        logger.warning(te)
+                        data = message_event(f"command timed out: {te}")
+                elif body.startswith('!ledger '):
+                    ledger_command = content['body'][8:]
+                    try:
+                        result = shell(ledger_command)
+                        data = message_event(result, f"<pre>{result}</pre>")
+                    except subprocess.CalledProcessError as cpe:
+                        logger.warning(cpe)
+                        data = message_event(f"command failed: {cpe}")
+                    except subprocess.TimeoutExpired as te:
+                        logger.warning(te)
+                        data = message_event(f"command timed out: {te}")
+                if data:
+                    matrix.send_message_event(room_id, "m.room.message", data)
+            else:
+                matrix.send_message(room_id,
+                                    "I'm sorry, but this is not a private room so I'm not going to do that.")
+        logger.debug("------------------------------------------------------------------")
+
     return flask.jsonify({})
 
 
-def shell(command) -> bytes:
+def message_event(body, formatted_body=None) -> dict:
+    me = {
+        "format": "org.matrix.custom.html",
+        "msgtype": "m.text",
+        'body': body
+    }
+    if formatted_body:
+        me['formatted_body'] = formatted_body
+
+    return me
+
+
+def shell(command) -> str:
     cmd = [
         "/bin/sh",
         "-c",
         command
     ]
-    try:
-        out = subprocess.check_output(cmd)
-        logger.debug(out)
-        return out
-    except subprocess.CalledProcessError as cpe:
-        logger.error(cpe)
+    return subprocess.check_output(cmd, timeout=13).decode()
+
+
+def get_joined_room_members(room_id) -> dict:
+    url = f"{HS_URL}/_matrix/client/r0/rooms/{room_id}/joined_members?access_token={AS_TOKEN}"
+    get = requests.get(url)
+    return get.json()['joined']
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0")
-    # matrix.get_room_members("#superior:matrix.rosenberg-watt.com")
+    waitress.serve(app)
